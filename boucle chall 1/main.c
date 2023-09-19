aa15f7a4d8901c4ced9465846fdf0a50a42e1793
Oussama Bel-kaze <oussamaazrour@gmail.com>
2023-09-19 09:23:12 +0100

    boucle chall 2

--- boucle chall 1/main.c
+++ boucle chall 1/main.c
@@ -4,11 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("saisie un nombre : \n");
-	int n,i;
-	scanf("%d",&n);
-	for(i=1;i<=10;i++){
-		printf("%d*%d=%d \n",n,i,n*i);
-	} 
+	
 	return 0;
 }
--- boucle chall 2/main.c
+++ boucle chall 2/main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main() {
 	int n;
 	int y=1,i,j,k,x;
 	printf("Entrer nombre : ");
